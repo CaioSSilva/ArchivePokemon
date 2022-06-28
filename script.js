0c9ba1bff1906ed36e34ddc44c8695b967d0fc51
Caio Souza Silva <caiosouzasilva13650@gmail.com>
2022-06-28 00:30:31 -0300

    Refactoring...

--- script.js
+++ script.js
@@ -48,13 +48,6 @@ function search(){
             alert("Nome ou Numero não encontrado"); 
         })
 }
-//Verifica escrita do campo de pesquisa  
-function searchVerify(){
-    const campoPesquisa = document.querySelector('.search').value
-    if(campoPesquisa.length == 0){
-        Promise.all(pokemonPomises).then(generateHTML).then(insertPokemons)
-    }
-}
 //Mostrando Pokemon
 function pokeShow(id){
     fetch(getpokemonUrl(id))
@@ -142,43 +135,17 @@ function pokeShow(id){
 }
 //Voltar Home
 function backHome(){
-    const page = document.body
-    page.innerHTML = `
-    <h1>Pokedex</h1>
-    <p>Search for Pokémon by name or using the National Pokédex number.</p>
-    <div class="search-menu-content">
-        <input class="search" placeholder="What Pokémon are you looking for?" oninput="searchVerify()"/>
-        <button class="search-btn" onclick="search()"><img src="https://cdn-icons-png.flaticon.com/512/54/54481.png" alt="Pesquisar"></button>
-    </div>
-    <ul data-js="pokedex" class="pokedex"></ul>`
-
-    const getpokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
-
-    const generatePokemonPromises = () => Array(50).fill().map((_,index)=>fetch(getpokemonUrl(index + 1)).then(response => response.json()))
-
-    const generateHTML = pokemons => pokemons.reduce((accumulator,{name, id, types}) => {const Elementtypes = types.map(typeinfo => typeinfo.type.name)
-    
-        accumulator += `
-        <li class="card ${Elementtypes[0]}" onclick="pokeShow(${id})">
-            <img class="card-image" alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png"/>
-            <h2 class="card-title">${id}. ${name}</h2>
-            <p class="card-subtitle ${Elementtypes[0] + '-element-container'}">${Elementtypes.join(' & ')}</p>
-        </li>`
-        return accumulator
-    }, '')
-
-
-    const insertPokemons = pokemons =>{
-    const ul = document.querySelector('[data-js="pokedex"]')
-    
-        ul.innerHTML = pokemons
-    }
-        
-    const pokemonPomises = generatePokemonPromises()
-    
-    Promise.all(pokemonPomises).then(generateHTML).then(insertPokemons)
+    window.location.reload();
 }
 //Voltar ao topo quando carregar pokemon
 function scrollReset(){
     window.scrollTo(0, 0);
 }
+const campoPesquisa = document.querySelector('.search')
+
+campoPesquisa.addEventListener("keydown", (key) =>{
+    if (key.code === "Enter") {
+        search();
+    }
+})
+    
