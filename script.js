309e2c72c7df42f350393c7bf98051f4c37b63d9
Caio Souza Silva <caiosouzasilva13650@gmail.com>
2022-06-29 01:32:00 -0300

    add pokemon information

--- script.js
+++ script.js
@@ -58,9 +58,7 @@ function pokeShow(id){
         .then(promises => promises.json())
         .then((pokemon) =>{
             const page = document.body
-            const statusBaseExperience = pokemon.base_experience
             const statusWeight = pokemon.weight
-            const statusBaseValue = pokemon.stats.map(statsinfo => statsinfo.base_stat)
             const Elementtypes = pokemon.types.map(typeinfo => typeinfo.type.name) 
             page.innerHTML = `
             <div class="pokemonPageContainer">
@@ -81,56 +79,16 @@ function pokeShow(id){
                     </div>
                 </div>
                 <div class="other-stats-container">
-                    <div class="base-status-div">
-                        <div class="status-div">
-                            <div class=" icon-container ${Elementtypes[0] + '-element-container'}">
-                                <img class="icon-status" src="https://cdn-icons-png.flaticon.com/512/535/535234.png"/>
-                            </div>
-                            <h1 class="${Elementtypes[0] + '-color'}">HP</h1>
-                            <p>${statusBaseValue[0]}</p>
-                        </div>
-                        <div class="status-div">
-                            <div class="  icon-container ${Elementtypes[0] + '-element-container'}">
-                                <img class="icon-status" src="https://cdn-icons-png.flaticon.com/512/842/842082.png"/>
-                            </div>
-                            <h1 class="${Elementtypes[0] + '-color'}">ATTACK</h1>
-                            <p>${statusBaseValue[1]}</p>
-                        </div>
-                        <div class="status-div">
-                            <div class=" icon-container  ${Elementtypes[0] + '-element-container'}">
-                                <img class="icon-status" src="https://cdn-icons-png.flaticon.com/512/786/786346.png"/>
-                            </div>
-                            <h1 class="${Elementtypes[0] + '-color'}">DEFENSE</h1>
-                            <p>${statusBaseValue[2]}</p>
-                        </div>
-                        <div class="status-div">
-                            <div class=" icon-container  ${Elementtypes[0] + '-element-container'}">
-                                <img class="icon-status" src="https://cdn-icons-png.flaticon.com/512/6997/6997647.png"/>
-                            </div>
-                            <h1 class="${Elementtypes[0] + '-color'}">SPECIAL-ATTACK</h1>
-                            <p>${statusBaseValue[3]}</p>
-                        </div>
-                        <div class="status-div">
-                            <div class=" icon-container  ${Elementtypes[0] + '-element-container'}">
-                                <img class="icon-status" src="https://cdn-icons-png.flaticon.com/512/2919/2919703.png"/>
-                            </div>
-                            <h1 class="${Elementtypes[0] + '-color'}">SPECIAL-DEFENSE</h1>
-                            <p>${statusBaseValue[4]}</p>
-                        </div>
-                        <div class="pokedex-data">
-                            <div class=" icon-container  ${Elementtypes[0] + '-element-container'}">
-                                <img class="icon-status" src="https://cdn-icons-png.flaticon.com/512/626/626075.png"/>
-                            </div>
-                            <h1 class="${Elementtypes[0] + '-color'}">BASE-XP</h1>
-                            <p>${statusBaseExperience}</p>
-                        </div>
-                        <div class="pokedex-data">
-                            <div class=" icon-container  ${Elementtypes[0] + '-element-container'}">
-                                <img class="icon-status" src="https://cdn-icons-png.flaticon.com/512/65/65512.png"/>
-                            </div>
-                            <h1 class="${Elementtypes[0] + '-color'}">WEIGHT</h1>
-                            <p>${statusWeight}</p>
-                        </div>
+                    <nav id="menus-container">
+                        <ul>
+                            <li class="menu-title ${Elementtypes[0] + '-color'}"onclick="about(${id})">About</li>
+                            <li class="menu-title ${Elementtypes[0] + '-color'}"onclick="baseStatus(${id})">Base Stats</li>
+                            <li class="menu-title ${Elementtypes[0] + '-color'}"onclick="evolution(${id})">Evolution</li>
+                            <li class="menu-title ${Elementtypes[0] + '-color'}"onclick="moves(${id})">Movements</li>
+                        </ul>
+                    </nav>
+                    <div id="about-div">
+                        ${about(id)}
                     </div>
                 </div>
             </div>`
@@ -155,10 +113,10 @@ campoPesquisa.addEventListener("keydown", (key) =>{
 //Loading
 function showLoading() {
     const loading = document.getElementById('pokeball-loading')
-    loading.classList.add('active')
+    loading.classList.add('.active')
     
     setTimeout(() => {
-        loading.classList.remove('active')
+        loading.classList.remove('.active')
         setTimeout(() => {
             pokemonsNumber = pokemonsNumber + 6
             const pokemonPomises = generatePokemonPromises()
@@ -175,4 +133,105 @@ window.addEventListener('scroll', ()=>{
 //backtop-button
 function backTop(){
     document.documentElement.scrollTop = 0
+}
+//About Pokemon
+function about(id){
+    fetch(getpokemonUrl(id))
+        .then(promises => promises.json())
+        .then((pokemon) =>{
+            const statusWeight = pokemon.weight
+            const Elementtypes = pokemon.types.map(typeinfo => typeinfo.type.name)
+            const forms = pokemon.forms.map(forms => forms.name).join(' & ')
+            const height = pokemon.height
+            const habilities = pokemon.abilities.map(ability => ability.ability.name).join(' & ')
+            document.getElementById('about-div').innerHTML =
+            `<div class="about-data">
+                <div class="species-container">
+                    <h1 class="${Elementtypes[0] + '-color'}">FORMS</h1>
+                    <p>${forms}</p>
+                </div>
+                <div class="height-container">
+                    <h1 class="${Elementtypes[0] + '-color'}">HEIGHT</h1>
+                    <p>${height}</p>
+                </div>
+                <div class="weight-container">
+                    <h1 class="${Elementtypes[0] + '-color'}">WEIGHT</h1>
+                    <p>${statusWeight}</p>
+                </div>
+                <div class="abilities-container">
+                    <h1 class="${Elementtypes[0] + '-color'}">ABILITIES</h1>
+                    <p>${habilities}</p>
+                </div>
+            </div>`
+        })
+}
+//Status Base Pokemon
+function baseStatus(id){
+    fetch(getpokemonUrl(id))
+        .then(promises => promises.json())
+        .then((pokemon) =>{
+            const statusBaseExperience = pokemon.base_experience
+            const statusBaseValue = pokemon.stats.map(statsinfo => statsinfo.base_stat)
+            const Elementtypes = pokemon.types.map(typeinfo => typeinfo.type.name) 
+            document.getElementById('about-div').innerHTML =
+
+            `<div class="status-div">
+                <div class=" icon-container ${Elementtypes[0] + '-element-container'}">
+                    <img class="icon-status" src="https://cdn-icons-png.flaticon.com/512/535/535234.png"/>
+                </div>
+                <h1 class="${Elementtypes[0] + '-color'}">HP</h1>
+                <p>${statusBaseValue[0]}</p>
+            </div>
+            <div class="status-div">
+                <div class="  icon-container ${Elementtypes[0] + '-element-container'}">
+                    <img class="icon-status" src="https://cdn-icons-png.flaticon.com/512/842/842082.png"/>
+                </div>
+                <h1 class="${Elementtypes[0] + '-color'}">ATTACK</h1>
+                <p>${statusBaseValue[1]}</p>
+            </div>
+            <div class="status-div">
+                <div class=" icon-container  ${Elementtypes[0] + '-element-container'}">
+                    <img class="icon-status" src="https://cdn-icons-png.flaticon.com/512/786/786346.png"/>
+                </div>
+                <h1 class="${Elementtypes[0] + '-color'}">DEFENSE</h1>
+                <p>${statusBaseValue[2]}</p>
+            </div>
+            <div class="status-div">
+                <div class=" icon-container  ${Elementtypes[0] + '-element-container'}">
+                    <img class="icon-status" src="https://cdn-icons-png.flaticon.com/512/6997/6997647.png"/>
+                </div>
+                <h1 class="${Elementtypes[0] + '-color'}">SPECIAL-ATTACK</h1>
+                <p>${statusBaseValue[3]}</p>
+            </div>
+            <div class="status-div">
+                <div class=" icon-container  ${Elementtypes[0] + '-element-container'}">
+                    <img class="icon-status" src="https://cdn-icons-png.flaticon.com/512/2919/2919703.png"/>
+                </div>
+                <h1 class="${Elementtypes[0] + '-color'}">SPECIAL-DEFENSE</h1>
+                <p>${statusBaseValue[4]}</p>
+            </div>
+            <div class="pokedex-data">
+                <div class=" icon-container  ${Elementtypes[0] + '-element-container'}">
+                    <img class="icon-status" src="https://cdn-icons-png.flaticon.com/512/626/626075.png"/>
+                </div>
+                <h1 class="${Elementtypes[0] + '-color'}">BASE-XP</h1>
+                <p>${statusBaseExperience}</p>
+            </div>`
+        }  
+    )
+}
+//Evolution Pokemon
+function evolution(id){
+    
+}
+//Moves Pokemon
+function moves(id){
+    fetch(getpokemonUrl(id))
+        .then(promises => promises.json())
+        .then((pokemon) =>{
+            const Elementtypes = pokemon.types.map(typeinfo => typeinfo.type.name)
+            const moves = pokemon.moves.map(move => move.move.name).join('<p/>');
+            document.getElementById('about-div').innerHTML =
+            `<div class="moves-div ${Elementtypes[0] + '-element-container'}"><p>${moves}</p></div>`
+        })
 }
